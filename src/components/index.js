c19f9ab2e42b2a0124e73a569e825395c0f51a37
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2023-04-09 13:09:03 +0200

    feat: add new component, and add new demo

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,13 @@ import './button-play-stop/button-play-stop.js';
 import './button-active/button-active.js';
 import './round-cicles/round-cicle.js';
 import './chrono-view/chrono-view.js';
+import './start-component/start-component.js';
 
 // demos
 import './button-play-stop/demo/button-play-stop-demo.js';
 import './button-active/demo/button-active-demo.js';
 import './round-cicles/demo/round-cicle-demo.js';
 import './chrono-view/demo/chrono-view-demo.js';
+
+// main-components
+import './footer-component/footer-component.js';
