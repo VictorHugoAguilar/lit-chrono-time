0ee1c9327231b4e361c06e2409da9c22fb150cbe
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2023-04-09 14:38:44 +0200

    fix: changes config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,7 @@
+// prettier.config.js or .prettierrc.js
 module.exports = {
-  singleQuote: trye,
-  arrowParens: "avoid",
-
-}
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+};
