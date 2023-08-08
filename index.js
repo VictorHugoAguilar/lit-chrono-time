724e2577a9e2e07095e98a6002ba4cc5a1aee317
Victor Hugo Aguilar <victorhugoaguilar@aol.com>
2023-08-08 14:25:17 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 window.addEventListener('load', () => {
 
-  
   setTimeout( () => {
     const containerUser = document.querySelector('#user');
     const containerTsec = document.querySelector('#tsec');
@@ -11,10 +10,9 @@ window.addEventListener('load', () => {
     const tsec = sessionStorage.getItem('tsec');
     const consumerId = sessionStorage.getItem('consumerId');
 
-
-    containerUser.innerHTML = JSON.stringify(user);
-    containerTsec.innerHTML = tsec;
-    containerConsumer.innerHTML = consumerId;
+    containerUser.innerHTML = user ? `User: ${JSON.stringify(user)}` : '';
+    containerTsec.innerHTML = tsec ? `TSEC: ${tsec}` :  '';
+    containerConsumer.innerHTML = consumerId ? `CONSUMERID: ${consumerId}` : '';
 
     if(user){
       containerImage.src = "https://master--steady-quokka-ad4380.netlify.app/assets/me-explota-la-cabeza.gif";
