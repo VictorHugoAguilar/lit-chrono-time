2ce37f3061fecbfb9fc37641714489fe5d1b5d31
Victor Hugo Aguilar <victorhugoaguilar@aol.com>
2023-08-09 14:14:38 +0200

    Add files via upload

--- assets/iframeShareSession.js
+++ assets/iframeShareSession.js
@@ -120,7 +120,7 @@ const onLoad = function onLoad() {
     const date = new Date();
     date.setTime(date.getTime() + daysToExpire * 24 * 60 * 60 * 1000);
     const expires = `expires=${date.toUTCString()}`;
-    document.cookie = `${name}=${value};${expires};path=/`;
+    window.cookie = `${name}=${value};${expires};path=/`;
   }
 
   function saveDataInSessionStorage(tsec, consumerId, loginUserInfo) {
