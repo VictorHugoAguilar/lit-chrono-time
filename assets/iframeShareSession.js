0da88831414c2efbdddc56c520717a091168feab
Victor Hugo Aguilar <victorhugoaguilar@aol.com>
2023-08-11 11:40:03 +0200

    Add files via upload

--- assets/iframeShareSession.js
+++ assets/iframeShareSession.js
@@ -303,6 +303,37 @@ const onLoad = function onLoad() {
     errorCb();
   });
 
+  // Write Javascript code!
+  const appDiv = document.getElementById('app');
+  appDiv.innerHTML = ``;
+
+  const claveInput = document.getElementById('clave');
+  const valorInput = document.getElementById('valor');
+  const getHandler = function () {
+    try {
+      const result = localStorage.getItem(claveInput.value);
+      appDiv.innerHTML = `${claveInput.value} vale ${result}`;
+    } catch {
+      appDiv.innerHTML = `error recuperando ${claveInput.value}`;
+    }
+  };
+  const setHandler = function () {
+    try {
+      localStorage.setItem(claveInput.value, valorInput.value);
+      appDiv.innerHTML = `${claveInput.value} guardado`;
+    } catch {
+      appDiv.innerHTML = `error recuperando ${claveInput.value}`;
+    }
+  };
+  const deleteHandler = function () {
+    try {
+      localStorage.removeItem(claveInput.value);
+      appDiv.innerHTML = `${claveInput.value} borrado`;
+    } catch {
+      appDiv.innerHTML = `error recuperando ${claveInput.value}`;
+    }
+  };
+
   const navigateHandler = function () {
     window.top.window.location.href =
       'https://master--steady-quokka-ad4380.netlify.app';
@@ -310,6 +341,15 @@ const onLoad = function onLoad() {
 
   const navigateBtn = document.getElementById('navigate');
   navigateBtn.addEventListener('click', navigateHandler, false);
+
+  const getBtn = document.getElementById('get');
+  getBtn.addEventListener('click', getHandler, false);
+
+  const setBtn = document.getElementById('set');
+  setBtn.addEventListener('click', setHandler, false);
+
+  const deleteBtn = document.getElementById('delete');
+  deleteBtn.addEventListener('click', deleteHandler, false);
 };
 
 // print data when an event is received
